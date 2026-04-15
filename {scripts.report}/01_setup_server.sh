53228b3b2345271c18865d80dd4c3950075b40bb
Nicola Saracino <nicola.saracino098@gmail.com>
2026-04-15 18:36:30 +0200

    Setup: creazione ambiente remoto sul server
    Creazione script 01_server.sh. Connessione SSH effettuata e creata l'impalcatura delle directory di lavoro (01_QC, 02_Alignment, 03_VariantCalling, 04_Results) nella home del server.

---
+++ {scripts.report}/01_setup_server.sh
@@ -0,0 +1,3 @@
+# Connessione al server effettuata come BCG2025_saracinoN
+# Creazione della struttura di directory per il progetto 2025 nella home
+mkdir -p ~/Genomics_Project/{01_QC,02_Alignment,03_VariantCalling,04_Results}
