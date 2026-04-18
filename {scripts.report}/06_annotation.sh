18dcc4468373278a2618aefc45c4e74ae1dd70f2
Nicola Saracino <nicola.saracino098@gmail.com>
2026-04-18 17:11:38 +0200

    Fix: switch da VEP offline a VEP database online
    Risolto errore di cache inesistente (home_sapiens non trovato sul server locale) convertendo l'annotazione VEP alla modalità --database. Il ridotto numero di varianti filtrate permette una query remota efficiente verso i server pubblici.

--- {scripts.report}/06_annotation.sh
+++ {scripts.report}/06_annotation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# FASE 6: Annotazione funzionale delle varianti
-# Eseguito sul server il $(date)
+# FASE 6: Annotazione funzionale delle varianti tramite Ensembl REST/Database
+# Sostituito --offline con --database causa assenza di cache locale
 
 VCF_DIR="$HOME/Genomics_Project/03_VariantCalling"
 ANN_DIR="$HOME/Genomics_Project/04_Annotation"
@@ -11,5 +11,5 @@ cd ${VCF_DIR}
 for case in 610 683 657 586 681; do
     vep -i case${case}_final.vcf \
         -o ${ANN_DIR}/case${case}_annotated.txt \
-        --tab --symbol --hgvs --canonical --offline --force_overwrite
+        --tab --symbol --hgvs --canonical --database --force_overwrite
 done
