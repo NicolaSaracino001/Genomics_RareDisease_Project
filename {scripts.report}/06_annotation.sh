da2c0155650742373ece2e865f0391e304b004ce
Nicola Saracino <nicola.saracino098@gmail.com>
2026-04-18 17:34:37 +0200

    Fix bypassato firewall usando cache offline condivisa di VEP
    Sostituita la query online al database (bloccata dal firewall di ateneo) con l'opzione --offline, puntando la cache locale del docente. generazione file txt completata con successo.

--- {scripts.report}/06_annotation.sh
+++ {scripts.report}/06_annotation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# FASE 6: Annotazione funzionale delle varianti tramite Ensembl REST/Database
-# Sostituito --offline con --database causa assenza di cache locale
+# FASE 6: Annotazione funzionale delle varianti
+# Utilizzo della cache offline condivisa dal docente per bypassare i blocchi di rete
 
 VCF_DIR="$HOME/Genomics_Project/03_VariantCalling"
 ANN_DIR="$HOME/Genomics_Project/04_Annotation"
@@ -11,5 +11,5 @@ cd ${VCF_DIR}
 for case in 610 683 657 586 681; do
     vep -i case${case}_final.vcf \
         -o ${ANN_DIR}/case${case}_annotated.txt \
-        --tab --symbol --hgvs --canonical --database --force_overwrite
+        --tab --symbol --hgvs --canonical --offline --dir_cache /home/mchiara/.vep --force_overwrite
 done
